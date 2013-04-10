ae3f363712d20c19b21f0f99c59426bb3b1e4fcc
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2013-04-11 01:12:23 +0900

    update example

--- _examples/main.go
+++ _examples/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-  "github.com/t-k/fluent-logger-golang"
-  "fmt"
+	"fmt"
+	"github.com/t-k/fluent-logger-golang"
 )
 
 func main() {
-  logger := fluent.New(fluent.Config{})
-  err := logger.Connect()
-  if err != nil {
-    fmt.Println(err)
-  }
-  defer logger.Close()
-  tag := "myapp.access"
-  message := "testing"
-  logger.Post(tag, message)
+	logger := fluent.New(fluent.Config{FluentPort: 24224, FluentHost: "127.0.0.1"})
+	err := logger.Connect()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer logger.Close()
+	tag := "myapp.access"
+	var data = map[string]string{
+		"foo":  "bar",
+		"hoge": "hoge"}
+	logger.Post(tag, data)
 }
