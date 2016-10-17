10360f72d92875717c8d4b57272a7c745350696a
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-10-17 20:41:31 +0900

    fix example to post logs continuously (to simulate connect/disconnect/reconnect)

--- _examples/main.go
+++ _examples/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/fluent/fluent-logger-golang/fluent"
-	// "../fluent"
+	"time"
+
+	"../fluent"
+
+	"github.com/Sirupsen/logrus"
 )
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	logger, err := fluent.New(fluent.Config{FluentPort: 24224, FluentHost: "127.0.0.1"})
 	if err != nil {
 		fmt.Println(err)
@@ -18,7 +22,13 @@ func main() {
 		"hoge": "hoge"}
 	i := 0
 	for i < 100 {
-		logger.Post(tag, data)
+		e := logger.Post(tag, data)
+		if e != nil {
+			logrus.Debugf("Error while posting log: %s", e)
+		} else {
+			logrus.Debug("Success to post log")
+		}
 		i = i + 1
+		time.Sleep(1000 * time.Millisecond)
 	}
 }
